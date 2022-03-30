06a8361586732aa1e77d8f20706dacb1d4e5043f
kovus380 <kosaf888@gmail.com>
2022-03-31 06:06:32 +0900

    Add Toast Message - side song

--- app/src/main/java/com/example/practice/AlbumFragment.kt
+++ app/src/main/java/com/example/practice/AlbumFragment.kt
@@ -21,9 +21,25 @@ class AlbumFragment : Fragment() {
         binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
         }
-        binding.songLalacLayout.setOnClickListener {
-            Toast.makeText(activity, "LILAC", Toast.LENGTH_SHORT).show()
+        binding.songLilacLayout.setOnClickListener {
+            Toast.makeText(activity, "라일락", Toast.LENGTH_SHORT).show()
         }
+        binding.songFluLayout.setOnClickListener {
+            Toast.makeText(activity, "Flu", Toast.LENGTH_SHORT).show()
+        }
+        binding.songCoinLayout.setOnClickListener {
+            Toast.makeText(activity, "Coin", Toast.LENGTH_SHORT).show()
+        }
+        binding.songSpringLayout.setOnClickListener {
+            Toast.makeText(activity, "봄 안녕 봄", Toast.LENGTH_SHORT).show()
+        }
+        binding.songCelebrityLayout.setOnClickListener {
+            Toast.makeText(activity, "Celebrity", Toast.LENGTH_SHORT).show()
+        }
+        binding.songSingLayout.setOnClickListener {
+            Toast.makeText(activity, "돌림노래(Feat. Dean)", Toast.LENGTH_SHORT).show()
+        }
+
         return binding.root
     }
 }
