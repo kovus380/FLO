2cf71248d5e03a271d24fa945820b888b9281c1d
kovus380 <kosaf888@gmail.com>
2022-04-07 02:12:25 +0900

    tablayout & viewpager

--- app/src/main/java/com/example/practice/AlbumFragment.kt
+++ app/src/main/java/com/example/practice/AlbumFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.practice.databinding.FragmentAlbumBinding
 import com.example.prcatice.HomeFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
-    lateinit var binding : FragmentAlbumBinding
+    lateinit var binding: FragmentAlbumBinding
+    private var information = arrayListOf("수록곡", "상세정보", "영상")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,46 +22,17 @@ class AlbumFragment : Fragment() {
         binding = FragmentAlbumBinding.inflate(inflater, container, false)
 
         binding.albumBackIv.setOnClickListener {
-            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
-        }
-        binding.songLilacLayout.setOnClickListener {
-            Toast.makeText(activity, "라일락", Toast.LENGTH_SHORT).show()
-        }
-        binding.songFluLayout.setOnClickListener {
-            Toast.makeText(activity, "Flu", Toast.LENGTH_SHORT).show()
-        }
-        binding.songCoinLayout.setOnClickListener {
-            Toast.makeText(activity, "Coin", Toast.LENGTH_SHORT).show()
-        }
-        binding.songSpringLayout.setOnClickListener {
-            Toast.makeText(activity, "봄 안녕 봄", Toast.LENGTH_SHORT).show()
-        }
-        binding.songCelebrityLayout.setOnClickListener {
-            Toast.makeText(activity, "Celebrity", Toast.LENGTH_SHORT).show()
-        }
-        binding.songSingLayout.setOnClickListener {
-            Toast.makeText(activity, "돌림노래(Feat. Dean)", Toast.LENGTH_SHORT).show()
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, HomeFragment())
+                .commitAllowingStateLoss()
         }
 
-        binding.songMixoffTg.setOnClickListener {
-            setMixStatus(false)
-        }
-
-        binding.songMixonTg.setOnClickListener {
-            setMixStatus(true)
-        }
-
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp) {
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
         return binding.root
-    }
-    fun setMixStatus(isMix : Boolean) {
-        if(isMix) {
-            binding.songMixoffTg.visibility = View.VISIBLE
-            binding.songMixonTg.visibility = View.GONE
-        }
-        else {
-            binding.songMixoffTg.visibility = View.GONE
-            binding.songMixonTg.visibility = View.VISIBLE
-
-        }
     }
 }
