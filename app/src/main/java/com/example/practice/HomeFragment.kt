e15de8a9dfb82f34a6047da1a9c20d15d78b5cd5
kovus380 <kosaf888@gmail.com>
2022-04-07 01:05:15 +0900

    home banner - viewpager

--- app/src/main/java/com/example/practice/HomeFragment.kt
+++ app/src/main/java/com/example/practice/HomeFragment.kt
@@ -5,9 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.practice.AlbumFragment
-import com.example.practice.MainActivity
-import com.example.practice.R
+import androidx.viewpager2.widget.ViewPager2
+import com.example.practice.*
 import com.example.practice.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -24,6 +23,12 @@ class HomeFragment : Fragment() {
         binding.homeTodayReleaseAlbumImageIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
         }
+
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         return binding.root
     }
 }
