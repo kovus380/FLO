2a1acd6d291b3e3f01e630cad38d930cfe4bce79
kovus380 <kosaf888@gmail.com>
2022-05-18 23:29:28 +0900

    Implement LogIn & LogOut

--- app/src/main/java/com/example/practice/LoginActivity.kt
+++ app/src/main/java/com/example/practice/LoginActivity.kt
@@ -2,7 +2,10 @@ package com.example.practice
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.flo.SongDatabase
 import com.example.practice.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -16,5 +19,46 @@ class LoginActivity : AppCompatActivity() {
         binding.loginSignUpTv.setOnClickListener {
             startActivity(Intent(this, SingUpActivity::class.java))
         }
+
+        binding.loginSignInBtn.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        if (binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (binding.loginPasswordEt.text.toString().isEmpty()){
+            Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val email: String = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
+        val pwd: String = binding.loginPasswordEt.text.toString()
+        val songDB = SongDatabase.getInstance(this)!!
+        val user = songDB.userDao().getUser(email, pwd)
+
+        user?.let{
+            Log.d("LOGIN_ACT/GET_USER", "userId : ${user.id}, $user")
+            saveJwt(user.id)
+            startMainActivity()
+        }
+        Toast.makeText(this, "회원 정보가 존재하지 않습니다", Toast.LENGTH_SHORT).show()
+
+
+    }
+
+    private fun saveJwt(jwt: Int){
+        val spf = getSharedPreferences("auth", MODE_PRIVATE)
+        val editor = spf.edit()
+        editor.putInt("jwt", jwt)
+        editor.apply()
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
