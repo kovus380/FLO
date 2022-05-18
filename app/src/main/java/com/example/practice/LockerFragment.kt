e3374ec90936a32cefc3d3e5fea2ae504f06a8bf
kovus380 <kosaf888@gmail.com>
2022-05-19 00:28:39 +0900

    Implement Album Like

--- app/src/main/java/com/example/practice/LockerFragment.kt
+++ app/src/main/java/com/example/practice/LockerFragment.kt
@@ -23,7 +23,7 @@ private const val ARG_PARAM2 = "param2"
 class LockerFragment : Fragment() {
 
     lateinit var binding : FragmentLockerBinding
-    private val information = arrayListOf("저장한 곡", "음악파일")
+    private val information = arrayListOf("저장한 곡", "음악파일", "저장앨범")
     // TODO: Rename and change types of parameters
 //    private var param1: String? = null
 //    private var param2: String? = null
--- app/src/main/java/com/example/practice/LockerVPAdapter.kt
+++ app/src/main/java/com/example/practice/LockerVPAdapter.kt
@@ -2,14 +2,16 @@ package com.example.practice
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.flo.SavedAlbumFragment
 
 class LockerVPAdapter(fragment: Fragment) : FragmentStateAdapter(fragment){
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
         return when(position){
             0 -> StoredSongFragment()
-            else -> FileFragment()
+            1 -> FileFragment()
+            else -> SavedAlbumFragment()
         }
     }
 
