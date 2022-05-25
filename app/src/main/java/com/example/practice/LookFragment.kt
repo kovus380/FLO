f97e78689561979bd84e0572171c6e767a892e47
kovus380 <kosaf888@gmail.com>
2022-05-26 02:34:19 +0900

    Song List API

--- app/src/main/java/com/example/practice/LookFragment.kt
+++ app/src/main/java/com/example/practice/LookFragment.kt
@@ -1,59 +1,58 @@
-package com.example.practice
+package com.example.flo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.practice.databinding.FragmentLookBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LookFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LookFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class LookFragment : Fragment(), LookView {
+    private lateinit var binding: FragmentLookBinding
+    private lateinit var floCharAdapter: SongRVAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_look, container, false)
+    ): View {
+        binding = FragmentLookBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LookFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LookFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        getSongs()
+    }
+
+    private fun initRecyclerView(result: FloChartResult) {
+        floCharAdapter = SongRVAdapter(requireContext(), result)
+
+        binding.lookFloChartRv.adapter = floCharAdapter
+    }
+
+    private fun getSongs() {
+        val songService = SongService()
+        songService.setLookView(this)
+
+        songService.getSongs()
+
+    }
+
+    override fun onGetSongLoading() {
+        binding.lookLoadingPb.visibility = View.VISIBLE
+    }
+
+    override fun onGetSongSuccess(code: Int, result: FloChartResult) {
+        binding.lookLoadingPb.visibility = View.GONE
+        initRecyclerView(result)
+    }
+
+    override fun onGetSongFailure(code: Int, message: String) {
+        binding.lookLoadingPb.visibility = View.GONE
+        Log.d("LOOK-FRAG/SONG-RESPONSE", message)
     }
 }
